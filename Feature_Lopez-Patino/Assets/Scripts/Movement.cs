82c3150ced623e7ec0597e740f510b83edad8a1e
Rodrigo <rlopezpatino@gmail.com>
2023-12-04 20:07:48 -0800

    small update
    made a to do list, and started the movement script

---
+++ Feature_Lopez-Patino/Assets/Scripts/Movement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Rodrigo, Lopez-Patino
+//11/21/23
+//set up Click to move
+
+
+public class Movement : MonoBehaviour
+{
+    //Variabels 
+    private bool isMoving = false;
+    private Vector3 currPos;
+    private Vector3 clickPos;
+
+}
---
+++ Feature_Lopez-Patino/Assets/Scripts/TODO.cs
@@ -0,0 +1,14 @@
+/*
+ *Click to move from League of Legends
+ *
+ *when the user clicks give show where on the map the click happened(Give feedback)
+ *get the location where the user clicked and store it
+ *move the character towards the clicked area
+ *if the user clicks some other location while the character is still moving:
+ * 1)stop moving towards first click location, from spot where stopped, start moving towards new click location 
+ * the camera will follow the moving character keeping it in center frame
+ * (optional) add camera toggle
+ *  if the player presess 'y' camera changes from follow to free cam
+ *  if player presses 'y' again  camera swithces back to follow 
+ *  while in free cam if the mouse gets near the screen edged the camera will move in the direction of the mouse
+ */
