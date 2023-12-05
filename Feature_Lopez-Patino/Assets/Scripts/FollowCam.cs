0563d6ab92802376bd08a700366c646e97b8e6ff
Rodrigo <rlopezpatino@gmail.com>
2023-12-05 11:45:13 -0800

    indicator for click works

---
+++ Feature_Lopez-Patino/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public FollowCam instance;
+
+    public GameObject poi;
+    public float camZ;
+    public float easing = 0.05f;
+
+    private void Awake()
+    {
+        instance = this;
+        camZ = this.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 destination  = poi.transform.position;
+
+        //interpolate beteween the cameras curent pos and the POI pos
+        destination = Vector3.Lerp(transform.position, destination, easing);
+
+        //move the camera
+        transform.position = destination;
+    }
+}
--- Feature_Lopez-Patino/Assets/Scripts/Movement.cs
+++ Feature_Lopez-Patino/Assets/Scripts/Movement.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements.Experimental;
 
 //Rodrigo, Lopez-Patino
 //11/21/23
@@ -11,25 +13,48 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     //Variabels 
-    private bool isMoving = false;
-    private bool followCam = true;
-    public GameObject spawnIndicator;
+    //private bool isMoving = false;
+    public GameObject Indicator;
+    public GameObject indicatorPrefab;
+    public GameObject player;
     private Vector3 currPos;
     private Vector3 clickPos;
+    private Camera mainCamera;
+    private float easing = 0.05f;
 
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
 
     private void FixedUpdate()
     {
-        
+
     }
 
     //get the location where the user clicked and store it
     private void OnMouseDown()
     {
-        clickPos = Input.mousePosition;
-        float x = clickPos.x;
-        float y = clickPos.y;
-
-        Debug.Log(x + " " + y);
+        //use ray to get the mouse position based on the camera 
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (Physics.Raycast(ray: ray, hitInfo: out RaycastHit hit) && hit.collider)
+        {
+            //if indicator is null place an indicator where the user clicked the mouse 
+            if (Indicator == null)
+            {
+                Indicator = Instantiate(indicatorPrefab);
+                Indicator.transform.position = hit.point;
+                clickPos = hit.point;
+            }
+            //if there is already an indicator and the user clicks again
+            //delete the previous indicatior and spawn a new one in the new location
+            if (Indicator != null)
+            {
+                Destroy(Indicator.gameObject);
+                Indicator = Instantiate(indicatorPrefab);
+                Indicator.transform.position = hit.point;
+                clickPos = hit.point;
+            }
+        }
     }
 }
