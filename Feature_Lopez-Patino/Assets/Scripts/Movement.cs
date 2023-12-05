166c72fc318ee5ec6a160c74bea1c1292be8ac1a
Rodrigo <rlopezpatino@gmail.com>
2023-12-04 22:14:12 -0800

    get mouse position

--- Feature_Lopez-Patino/Assets/Scripts/Movement.cs
+++ Feature_Lopez-Patino/Assets/Scripts/Movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 //Rodrigo, Lopez-Patino
@@ -11,7 +12,24 @@ public class Movement : MonoBehaviour
 {
     //Variabels 
     private bool isMoving = false;
+    private bool followCam = true;
+    public GameObject spawnIndicator;
     private Vector3 currPos;
     private Vector3 clickPos;
 
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    //get the location where the user clicked and store it
+    private void OnMouseDown()
+    {
+        clickPos = Input.mousePosition;
+        float x = clickPos.x;
+        float y = clickPos.y;
+
+        Debug.Log(x + " " + y);
+    }
 }
